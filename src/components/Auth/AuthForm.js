96f43f03e3620618a727a9e0cef8df1aaff5ab0f
S-123-satya <satyaprakash5056742@gmail.com>
2023-12-02 23:18:05 +0530

    dummy sending request printed

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,37 +1,43 @@
-import { useState, useRef } from 'react';
+import { useState, useRef } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [isSubmit, setIsSubmit] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
+  const submithandler = (e) => {
+    e.preventDefault();
+    setIsSubmit(true)
+    console.log(emailRef.current.value);
+    console.log(passwordRef.current.value);
+  };
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submithandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input ref={emailRef} type="email" id="email" required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input
-            type='password'
-            id='password'
-            required
-          />
+          <label htmlFor="password">Your Password</label>
+          <input ref={passwordRef} type="password" id="password" required />
         </div>
         <div className={classes.actions}>
+        {isSubmit?<button className={classes.toggle}>requesting sending</button> :<button type="submit">{isLogin ? "Login" : "Create account"}</button>}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
