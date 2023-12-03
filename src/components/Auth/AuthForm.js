7376035cda980878d33360e5a3d80eb8c2ae1508
S-123-satya <satyaprakash5056742@gmail.com>
2023-12-03 18:18:20 +0530

    authenticaion added

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 
 import classes from "./AuthForm.module.css";
+import axios from "axios";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -11,11 +12,27 @@ const AuthForm = () => {
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
-  const submithandler = (e) => {
+  const submithandler = async (e) => {
     e.preventDefault();
-    setIsSubmit(true)
-    console.log(emailRef.current.value);
-    console.log(passwordRef.current.value);
+    setIsSubmit(true);
+    let url;
+    const obj = {
+      password: passwordRef.current.value,
+      email: emailRef.current.value,
+      returnSecureToken: true,
+    };
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCxu324ZjtUtZVu_vfKSLRfZHtGouSdclo";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxu324ZjtUtZVu_vfKSLRfZHtGouSdclo";
+    }
+    const response = await axios.post(url,obj);
+    console.log(response);
+    passwordRef.current.value="";
+    emailRef.current.value="";
+    setIsSubmit(false);
   };
 
   return (
@@ -31,7 +48,13 @@ const AuthForm = () => {
           <input ref={passwordRef} type="password" id="password" required />
         </div>
         <div className={classes.actions}>
-        {isSubmit?<button className={classes.toggle}>requesting sending</button> :<button type="submit">{isLogin ? "Login" : "Create account"}</button>}
+          {isSubmit ? (
+            <button className={classes.toggle}>requesting sending</button>
+          ) : (
+            <button type="submit">
+              {isLogin ? "Login" : "Create account"}
+            </button>
+          )}
           <button
             type="button"
             className={classes.toggle}
